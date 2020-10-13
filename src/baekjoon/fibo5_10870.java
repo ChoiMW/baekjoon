b1acd94a869db83ad56b24d72ecac1ca6894f450
choiminwook <cmw0147@gmail.com>
2020-10-13 23:19:13 +0900

    no message

---
+++ src/baekjoon/fibo5_10870.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class fibo5_10870 {
+
+
+
+	    public static int fibo(int num){
+	        
+	        if(num==0){
+	            return 0;
+
+	        }
+	        else if(num == 1)
+	        {
+	            return 1;
+
+	        }
+	        else if(num == 2)
+	        {
+	            return 1;
+
+	        }
+	        else{
+	            return fibo(num-1) + fibo(num-2);
+
+	        }
+	        
+	    }
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			Scanner sc = new Scanner(System.in);
+
+	        int ret = fibo(sc.nextInt());
+			
+			System.out.println(ret);
+			
+		}
+
+
+
+}
